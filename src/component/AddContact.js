2dac93ac7b80d2aa73cc13bff700ecf84e287dbf
sagar wankhede <swankhede639@gmail.com>
2024-08-16 19:35:18 +0530

    added heading and form for fill the details first input for name and second input for email id

--- src/component/AddContact.js
+++ src/component/AddContact.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 function AddContact() {
   return (
-    <div>AddContact</div>
-  )
+    <React.Fragment>
+      <h1 className="add-contact">Add Contact</h1>
+    <div className="formHeader">
+      <form action="">
+        <label htmlFor="">Name:</label>
+        <br />
+        <input type="text" placeholder="Enter Name" name="name"  />
+        <br />
+        <label htmlFor="">Email:</label>
+        <br />
+        <input type="email" placeholder="Enter Email" name="email" />
+      </form>
+      <button  className="btn">Add Contact</button>
+    </div>
+    </React.Fragment>
+  );
 }
 
 export default AddContact
