45b79a57a306b279a18c49d6a306c712f21f3824
sagar wankhede <swankhede639@gmail.com>
2024-08-17 12:49:43 +0530

    added handleAdd function for getting values inside in console and this funciton pass to App.js

--- src/component/AddContact.js
+++ src/component/AddContact.js
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function AddContact() {
+function AddContact({addContact}) {
+
+  const [contactData,setContactData] = useState({name:"",email:""});
+
+  const handleChange = (e) => {
+    if(e.target.name === 'name'){
+      setContactData({...contactData, name:e.target.value})
+    }
+    else{
+      setContactData({...contactData,email: e.target.value})
+    }
+  }
+
+  const handleAdd = () => {
+    if(contactData.name === "" || contactData.email === ""){
+        alert('please fill all the details')
+        return;
+    }
+    //this console will show thie data that you entered 
+    // console.log(contactData);
+    addContact(contactData)
+    setContactData({name:"",email:""})
+}
+
+
   return (
     <React.Fragment>
       <h1 className="add-contact">Add Contact</h1>
@@ -8,13 +32,13 @@ function AddContact() {
       <form action="">
         <label htmlFor="">Name:</label>
         <br />
-        <input type="text" placeholder="Enter Name" name="name"  />
+        <input type="text" placeholder="Enter Name" name="name" value={contactData.name} onChange={handleChange} />
         <br />
         <label htmlFor="">Email:</label>
         <br />
-        <input type="email" placeholder="Enter Email" name="email" />
+        <input type="email" placeholder="Enter Email" name="email"value={contactData.email} onChange={handleChange}  />
       </form>
-      <button  className="btn">Add Contact</button>
+      <button onClick={handleAdd}  className="btn">Add Contact</button>
 
     </div>
     </React.Fragment>
