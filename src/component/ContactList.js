258ce2f1405a4defdb756ca8fcb1f0e003d6a9de
sagar wankhede <swankhede639@gmail.com>
2024-08-17 13:08:19 +0530

    create map method to iterate data map and create two div one div fo className contactsHeader for heading second div of className listRender for render data data send to App.js to ContactList.js create removeContact function for deleteing data

--- src/component/ContactList.js
+++ src/component/ContactList.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import DeleteIcon from "@mui/icons-material/Delete";
 
-function ContactList() {
+function ContactList(props) {
+  const {contact,removeContact} = props;
+  console.log(contact);
+
+  const contactList = contact.map((val,myId) => {
+    return (
+      <div key={myId} className="contacts">
+        <div className="name">{val.data.name}</div>
+        <div className="email">{val.data.email}</div>
+        <span onClick={() => removeContact(val.id)}>
+          <DeleteIcon />
+        </span>
+      </div>
+    );
+  });
   return (
-    <div>ContactList</div>
-  )
+    <React.Fragment>
+      <div className="contactsHeader">ContactList</div>
+      <div className="listRender">{contactList}</div>
+    </React.Fragment>
+  );
 }
 
-export default ContactList
+export default ContactList;
